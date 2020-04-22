b2b2c1c6a8d08cb049e044031e9ca13866614781
sweyome <sweyome1@gmail.com>
2020-04-21 20:59:47 -0400

    class work

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -8,24 +8,38 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
+        """
+        {
+            A:{},
+            B:{},
+            C:{},
+            D:{}
+
+        }
+        """
 
     def add_vertex(self, vertex_id):
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+      # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("that vertex does not exist")
+
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
